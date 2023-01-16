0f6cbfc92a3009ac13225c3380e1b8cd25cb6fd1
lintang81 <lintangkeren08@gmail.com>
2023-01-16 23:35:52 +0800

    deklarasi variabel dan tipe data

---
+++ Modul01/DataTypes.java
@@ -0,0 +1,13 @@
+public class DataTypes{
+    public static void main(String[] args){
+
+        String namaDepan = "Muhammmad";
+        String namaTengah = "Lintang";
+        String namaBelakang = "Herlambang";
+        int usia = 21;
+        int targetTahunKuliah = 4;
+        double ipk = 3.8;
+        char nilaiAbjad = 'A';
+        boolean tampan = true;
+    }
+}
