d1540b93e6a19b4d381ccae5074788fbe7607792
lintang81 <lintangkeren08@gmail.com>
2023-01-17 23:47:32 +0800

    Operasi Tipe String

--- Modul01/Modul01_IDEA/src/Main.java
+++ Modul01/Modul01_IDEA/src/Main.java
@@ -13,8 +13,27 @@ public class Main {
 
         System.out.println(namaDepan + namaTengah + namaBelakang);
 
-        char[] uniskaChar = {'U','N'.'I','S','K','A'};
+        char[] uniskaChar = {'U','N','I','S','K','A'};
         String uniskaString = new String(uniskaChar);
         System.out.println(uniskaString);
+
+
+
+
+        String namaLengkap = namaDepan + " " + namaTengah + " " + namaBelakang;
+        System.out.println(namaLengkap.length());
+        System.out.println(namaLengkap.indexOf("NIS"));
+        System.out.println(namaLengkap.substring(5));
+        System.out.println(namaLengkap.substring(5,12));
+        System.out.println(namaLengkap.replace("Lin", "Tang"));
+        System.out.println(namaLengkap.toUpperCase());
+        System.out.println(namaLengkap.toLowerCase());
+        System.out.println(namaLengkap.charAt(6));
+
+        String[] namaArray = namaLengkap.split(" ");
+
+        for (String nama : namaArray){
+            System.out.println(nama);
+        }
     }
 }
