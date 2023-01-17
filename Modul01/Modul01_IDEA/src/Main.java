32c02991297c3b8ec4ed7f7f2be9b9cc0f6f1543
lintang81 <lintangkeren08@gmail.com>
2023-01-17 23:31:21 +0800

    Project IDEA

---
+++ Modul01/Modul01_IDEA/src/Main.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        String namaDepan = "Muhammad";
+        String namaTengah = "Lintamg";
+        String namaBelakang = "Herlambang";
+        int usia = 21;
+        int targetTahunKuliah = 4;
+        double ipk = 3.8;
+        char nilaiAbjad = 'A';
+        boolean tampan = true;
+
+        System.out.println(namaDepan + namaTengah + namaBelakang);
+    }
+}
